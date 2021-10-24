e52de030208810c7f3079f927756dd1b857e42b2
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-10-24 22:55:52 +0300

    added css and images

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+require('./config/express-config')(app);
+require('./config/hbs-config')(app);
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('home', {layout: false})
 });
 
 app.listen(port, () => console.log(`App is running on http://localhost:${port}`));
